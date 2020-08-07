863f7d02819719edba39c434a61fda8135795793
Fabio Ono Tavoloni <fabioono25@gmail.com>
2020-08-07 23:36:54 +0100

    Shared Module + Divide routing modules

--- 1_AngularGetStarted_APM/src/app/app.module.ts
+++ 1_AngularGetStarted_APM/src/app/app.module.ts
@@ -1,39 +1,28 @@
-import { ProductDetailGuard } from './products/product-detail.guard';
 import { WelcomeComponent } from './home/welcome.component';
-import { StarComponent } from './shared/star.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
-import { ProductDetailComponent } from './products/product-detail.component';
+import { ProductModule } from './products/product.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    ProductDetailComponent,
     WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
-    RouterModule.forRoot([
-      {path: 'products', component: ProductListComponent},
-      {path: 'products/:id',
-        canActivate: [ProductDetailGuard],
-        component: ProductDetailComponent},
-      { path: 'welcome', component: WelcomeComponent },
-      {path: '', redirectTo: 'welcome', pathMatch: 'full'},
-      {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
-    ])
+    // RouterModule.forRoot([  //configure routes for our root component
+    //   { path: 'welcome', component: WelcomeComponent },
+    //   {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+    //   {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+    // ]),
+    ProductModule,
+    AppRoutingModule
   ],
   bootstrap: [AppComponent]
 })
