1d01d5157ee0d876ca8d987e66fffd973c0b6ea0
Fabio Ono Tavoloni <fabioono25@gmail.com>
2020-09-16 16:06:08 +0100

    minor changes

--- 6-Routing/src/app/home/welcome.component.ts
+++ 6-Routing/src/app/home/welcome.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-@Component({
+@Component({ //don't need the selector here, because we are routing directing, instead of using as a nested component
   templateUrl: './welcome.component.html'
 })
 export class WelcomeComponent {
