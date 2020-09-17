07ea73244d9b69d936c8c32ec0e0a2b6ef2d5a55
Fabio Ono Tavoloni <fabioono25@gmail.com>
2020-09-17 10:41:57 +0100

    route parameters

--- 6-Routing/src/app/app.module.ts
+++ 6-Routing/src/app/app.module.ts
@@ -20,7 +20,7 @@ import { MessageModule } from './messages/message.module';
   imports: [
     BrowserModule,
     HttpClientModule,
-    InMemoryWebApiModule.forRoot(ProductData, { delay: 1000 }),
+    InMemoryWebApiModule.forRoot(ProductData, { delay: 500 }),
     ProductModule,
     UserModule,
     MessageModule,
