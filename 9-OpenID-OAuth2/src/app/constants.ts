a1b92e539d865ad59e983b09d4c7c948663a3596
Fabio Ono Tavoloni <fabioono25@gmail.com>
2020-10-24 10:11:33 +0100

    configure authentication with Auth0

--- 9-OpenID-OAuth2/src/app/constants.ts
+++ 9-OpenID-OAuth2/src/app/constants.ts
@@ -3,12 +3,15 @@ export class Constants {
 	public static clientRoot = 'http://localhost:4200/';
 
 	public static apiRoot = 'https://securingangularappscoursev2-api.azurewebsites.net/api/';
-	public static stsAuthority = 'https://securingangularappscoursev2-sts.azurewebsites.net/';
+	//public static stsAuthority = 'https://securingangularappscoursev2-sts.azurewebsites.net/';
 
 	// public static apiRoot = 'http://localhost:2112/api/';
 	// public static stsAuthority = 'http://localhost:4242/';
 	// public static stsAuthority = 'https://briannoyes.auth0.com/';
+	public static stsAuthority = 'https://fabioono25.auth0.com/'; // subdomain for each tenant
 
-	public static clientId = 'spa-client';
+	// public static clientId = 'spa-client';
 	// public static clientId = 'Od8mbaoCGeXTF9ZWeQJspTTd7Qf1v1A7';
+
+	public static clientId = 'Naa72E9ITAkRy7CVwypaeE2on4qKJep0';
 }
