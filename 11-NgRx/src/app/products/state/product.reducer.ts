7a4d4b36df709f840065dd7c03e29bdbe83692ed
Fabio Ono Tavoloni <fabioono25@gmail.com>
2020-11-12 10:37:26 +0000

    create strongly-typed states

--- 11-NgRx/src/app/products/state/product.reducer.ts
+++ 11-NgRx/src/app/products/state/product.reducer.ts
@@ -1,8 +1,28 @@
 import { createAction, createReducer, on } from '@ngrx/store';
+import * as AppState from '../../state/app.state';
+import { Product } from '../product';
 
-export const productReducer = createReducer(
-  { showProductCode: true },
-  on(createAction('[Product] Toggle Product Code'), state => {
+// lazy loaded features support
+// entire state tree: this is the extended State, containing user and Product states
+export interface State extends AppState.State {
+  products: ProductState;
+}
+
+export interface ProductState {
+  showProductCode: boolean;
+  currentProduct: Product;
+  products: Product[];
+}
+
+const initialState: ProductState = {
+  showProductCode: true,
+  currentProduct: null,
+  products: []
+};
+
+export const productReducer = createReducer<ProductState>(
+  initialState,
+  on(createAction('[Product] Toggle Product Code'), (state): ProductState => {
     console.log(`original state ${JSON.stringify(state)}`);
     return {
       ...state,
