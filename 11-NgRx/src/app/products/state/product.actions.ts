c07e43528481a385e4c23b612097bcd443d3c257
Fabio Ono Tavoloni <fabioono25@gmail.com>
2020-11-12 22:08:12 +0000

    performing update and delete operations

--- 11-NgRx/src/app/products/state/product.actions.ts
+++ 11-NgRx/src/app/products/state/product.actions.ts
@@ -8,7 +8,7 @@ export const toggleProductCode = createAction(
 
 export const setCurrentProduct = createAction(
   '[Product] Set Current Product',
-  props<{ product: Product }>()
+  props<{ currentProductId: number }>()
 );
 
 export const clearCurrentProduct = createAction(
@@ -19,7 +19,6 @@ export const initializeCurrentProduct = createAction(
   '[Product] Initialize Current Product'
 );
 
-// working with complext operations
 export const loadProducts = createAction(
   '[Product] Load'
 );
@@ -31,5 +30,50 @@ export const loadProductsSuccess = createAction(
 
 export const loadProductsFailure = createAction(
   '[Product] Load Fail',
+  props<{ error: string }>()
+);
+
+export const updateProduct = createAction(
+  '[Product] Update Product',
+  props<{ product: Product }>()
+);
+
+export const updateProductSuccess = createAction(
+  '[Product] Update Product Success',
+  props<{ product: Product }>()
+);
+
+export const updateProductFailure = createAction(
+  '[Product] Update Product Fail',
+  props<{ error: string }>()
+);
+
+export const createProduct = createAction(
+  '[Product] Create Product',
+  props<{ product: Product }>()
+);
+
+export const createProductSuccess = createAction(
+  '[Product] Create Product Success',
+  props<{ product: Product }>()
+);
+
+export const createProductFailure = createAction(
+  '[Product] Create Product Fail',
+  props<{ error: string }>()
+);
+
+export const deleteProduct = createAction(
+  '[Product] Delete Product',
+  props<{ productId: number }>()
+);
+
+export const deleteProductSuccess = createAction(
+  '[Product] Delete Product Success',
+  props<{ productId: number }>()
+);
+
+export const deleteProductFailure = createAction(
+  '[Product] Delete Product Fail',
   props<{ error: string }>()
 );
