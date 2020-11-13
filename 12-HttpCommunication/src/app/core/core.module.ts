101b258e75535d6b30ea52ad5b4bf1803a66b1c6
Fabio Ono Tavoloni <fabioono25@gmail.com>
2020-11-13 18:19:18 +0000

    working with interceptors

--- 12-HttpCommunication/src/app/core/core.module.ts
+++ 12-HttpCommunication/src/app/core/core.module.ts
@@ -6,6 +6,9 @@ import { DataService } from './data.service';
 import { PlainLoggerService } from "./plain-logger.service";
 import { throwIfAlreadyLoaded } from "app/core/module-import-guard";
 import { BookTrackerErrorHandlerService } from './book-tracker-error-handler.service';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AddHeaderInterceptor } from './add-header.interceptor';
+import { LogResponseInterceptor } from './log-response.interceptor';
 
 @NgModule({
   imports: [
@@ -15,7 +18,9 @@ import { BookTrackerErrorHandlerService } from './book-tracker-error-handler.ser
   providers: [
     LoggerService,
     DataService,
-    { provide: ErrorHandler, useClass: BookTrackerErrorHandlerService }
+    { provide: ErrorHandler, useClass: BookTrackerErrorHandlerService },
+    { provide: HTTP_INTERCEPTORS, useClass: LogResponseInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: AddHeaderInterceptor, multi: true }
   ]
 })
 export class CoreModule {
