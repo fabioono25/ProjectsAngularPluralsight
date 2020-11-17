4f29feb0e8c275fb6c21e0a2e4cb8c46e4286bbe
Fabio Ono Tavoloni <fabioono25@gmail.com>
2020-11-17 12:18:07 +0000

    working with flex layout

--- 14-AngularMaterial/src/app/app.module.ts
+++ 14-AngularMaterial/src/app/app.module.ts
@@ -5,6 +5,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MaterialModule } from './shared/material.module';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'contactmanager', loadChildren: () => import('./contactmanager/contactmanager.module').then(m => m.ContactmanagerModule) },
+  { path: 'demo', loadChildren: () => import('./demo/demo.module').then(m => m.DemoModule) },
+  { path: '**', redirectTo: 'demo' }
+];
 
 @NgModule({
   declarations: [
@@ -13,7 +20,8 @@ import { MaterialModule } from './shared/material.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule
+    // MaterialModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
